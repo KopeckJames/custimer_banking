d8184c267171914ef8cd78c1714c048377431fef
KopeckJames <152207724+KopeckJames@users.noreply.github.com>
2024-06-03 18:19:44 -0500

    Update cd_account.py

--- cd_account.py
+++ cd_account.py
@@ -17,7 +17,7 @@ def create_cd_account(balance, interest_rate, months):
     cd_account = Account(balance, 0.0)
 
   # Calculate interest earned (assuming monthly interest accrual)
-    interest_earned = (balance * interest_rate * months / 12)/100
+    interest_earned = balance * (interest_rate / 100) * months / 12
 
   # Calculate the new balance
     new_balance = balance + interest_earned
